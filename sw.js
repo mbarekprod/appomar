3dbf444169c262b8edb7b12f07bad91da133ea03
mbarekprod <mbarekprod1@gmail.com>
2026-06-14 19:13:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,39 @@
-// ملف السيرفيس وركر لتفعيل التنزيل والإشعارات
-self.addEventListener('install', (event) => {
+self.addEventListener('install', (e) => {
     self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(self.clients.claim());
+self.addEventListener('activate', (e) => {
+    e.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', (event) => {
-    // يخلّي الموقع يخدم عادي
-    event.respondWith(fetch(event.request));
+// الاستماع للإشعارات القادمة
+self.addEventListener('push', (event) => {
+    let data = { title: 'مطعم الفنان 🍽️', body: 'ثمة جديد يستنى فيك!' };
+    if (event.data) {
+        try {
+            data = event.data.json();
+        } catch (e) {
+            data.body = event.data.text();
+        }
+    }
+
+    const options = {
+        body: data.body,
+        icon: 'go.png',
+        badge: 'go.png',
+        vibrate: [300, 100, 300],
+        data: { url: './index.html' }
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+// أول ما الحريف ينزل على الإشعار بـصبعة، يتحل الموقع
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow(event.notification.data.url)
+    );
 });
