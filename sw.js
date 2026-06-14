8cae5015d676d9352fe5faf8783fb3ad3dbba69e
mbarekprod <mbarekprod1@gmail.com>
2026-06-14 18:53:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
+// ملف السيرفيس وركر لتفعيل التنزيل والإشعارات
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
 
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    // يخلّي الموقع يخدم عادي
+    event.respondWith(fetch(event.request));
+});
